a1a2e85d19ca1ecc89b30ec718456a9312ef3afa
David <Dsheehe3@gmail.com>
2019-10-25 14:32:46 -0600

    Day3pt1 done

--- Day3.py
+++ Day3.py
@@ -1,27 +1,58 @@
 import pandas
 
-# dataset = pandas.read_csv("/home/david/Projects/advent/day3input.csv", header=None)
-
-dataset = open("/home/david/Projects/advent/day3input.txt")
+# open file
+dataset = open("day3input.txt")
 df = dataset.readlines()
 splits = list()
 
-for row in (df):
+for row in df:
     splits.append(row.split(" "))
 
 commasplit = list()
+lwdims = list()
 leftstart = list()
 topstart = list()
+widths = list()
+heights = list()
 
+# separate into locations and dimensions
 for list in splits:
     commasplit.append(list[2].split(","))
+    lwdims.append(list[3].split("x"))
 
+# separate into separate lists as integers after removing non-numerical characters
 for line in range(len(commasplit)):
     commasplit[line][1] = commasplit[line][1].rstrip(":")
+    leftstart.append(int(commasplit[line][0]))
+    topstart.append(int(commasplit[line][1]))
 
-print(splits)
-print(commasplit)
-print(commasplit[0][1])
+for line in range(len(lwdims)):
+    lwdims[line][1] = lwdims[line][1].rstrip("\n")
+    widths.append(int(lwdims[line][0]))
+    heights.append(int(lwdims[line][1]))
+
+# establis full cloth dimensions
+widetotal = max(leftstart) + max(widths)
+heighttotal = max(topstart) + max(heights)
+
+# initialize cloth 2D Array
+cloth = [[0 for i in range(widetotal+1)] for j in range(heighttotal+1)]
+
+# each claim of cloth on a specific location adds 1 to that location
+for i in range(len(splits)):
+    for j in range(widths[i]):
+        for k in range(heights[i]):
+            cloth[leftstart[i]+j][topstart[i]+k] += 1
+
+m = 0
+# find locations with more than 1 claim on it (location value >1)
+for i in range(widetotal):
+    for j in range(heighttotal):
+        if cloth[i][j] > 1:
+            m += 1
+print("Day 3 part 1 Answer:", m)
+
+
 
 
 
@@ -44,7 +75,10 @@ print(commasplit[0][1])
 #     The width of the rectangle in inches.
 #     The height of the rectangle in inches.
 #
-# A claim like #123 @ 3,2: 5x4 means that claim ID 123 specifies a rectangle 3 inches from the left edge, 2 inches from the top edge, 5 inches wide, and 4 inches tall. Visually, it claims the square inches of fabric represented by # (and ignores the square inches of fabric represented by .) in the diagram below:
+# A claim like #123 @ 3,2: 5x4 means that claim ID 123 specifies
+# a rectangle 3 inches from the left edge, 2 inches from the top edge, 5 inches wide, and 4 inches tall.
+# Visually, it claims the square inches of fabric represented by #
+# (and ignores the square inches of fabric represented by .) in the diagram below:
 #
 # ...........
 # ...........
---
+++ Day3Part2.py
@@ -0,0 +1,65 @@
+import pandas
+
+# open file
+dataset = open("day3input.txt")
+df = dataset.readlines()
+splits = list()
+
+for row in df:
+    splits.append(row.split(" "))
+
+commasplit = list()
+lwdims = list()
+leftstart = list()
+topstart = list()
+widths = list()
+heights = list()
+
+# separate into locations and dimensions
+for list in splits:
+    commasplit.append(list[2].split(","))
+    lwdims.append(list[3].split("x"))
+
+# separate into separate lists as integers after removing non-numerical characters
+for line in range(len(commasplit)):
+    commasplit[line][1] = commasplit[line][1].rstrip(":")
+    leftstart.append(int(commasplit[line][0]))
+    topstart.append(int(commasplit[line][1]))
+
+for line in range(len(lwdims)):
+    lwdims[line][1] = lwdims[line][1].rstrip("\n")
+    widths.append(int(lwdims[line][0]))
+    heights.append(int(lwdims[line][1]))
+
+# establis full cloth dimensions
+widetotal = max(leftstart) + max(widths)
+heighttotal = max(topstart) + max(heights)
+
+# initialize cloth 2D Array
+cloth = [[0 for i in range(widetotal+1)] for j in range(heighttotal+1)]
+
+# each claim of cloth on a specific location adds 1 to that location
+for i in range(len(splits)):
+    for j in range(widths[i]):
+        for k in range(heights[i]):
+            cloth[leftstart[i]+j][topstart[i]+k] += 1
+
+for i in range(len(splits)):
+    for j in range(widths[i]):
+        for k in range(heights[i]):
+            if cloth[leftstart[i]+j][topstart[i]+k] == 1:
+
+
+
+
+# m = 0
+# find locations with more than 1 claim on it (location value >1)
+# for i in range(widetotal):
+#     for j in range(heighttotal):
+#         if cloth[i][j] > 1:
+#             m += 1
+# print("Day 3 part 1 Answer:", m)
+
+
+
+
